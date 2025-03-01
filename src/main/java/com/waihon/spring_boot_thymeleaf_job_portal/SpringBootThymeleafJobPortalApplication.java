908db4cceb66199e96c9f00c6ed6c67886324949
Waihon Yew <yewwaihon@gmail.com>
2025-03-01 21:02:19 +0800

    Initial version generated by Spring Initializr

---
+++ src/main/java/com/waihon/spring_boot_thymeleaf_job_portal/SpringBootThymeleafJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.waihon.spring_boot_thymeleaf_job_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waihon/spring_boot_thymeleaf_job_portal/SpringBootThymeleafJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waihon.spring_boot_thymeleaf_job_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
