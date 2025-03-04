a231c21d586f85313e9d5115933130e48bce0b06
Waihon Yew <yewwaihon@gmail.com>
2025-03-05 05:32:53 +0800

    Controller, handler, and tempalte for Home

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/Home.java
@@ -0,0 +1,15 @@
+package com.waihon.springboot.thymeleaf.jobportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
+
