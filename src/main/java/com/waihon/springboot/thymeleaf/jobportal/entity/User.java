5b66c95514e01d847e5e611cbfbcd98bf2cc2f62
Waihon Yew <yewwaihon@gmail.com>
2025-03-07 23:05:00 +0800

    Create User class with private properties

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/entity/User.java
@@ -0,0 +1,32 @@
+package com.waihon.springboot.thymeleaf.jobportal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UserType userTypeId;
+
+}
