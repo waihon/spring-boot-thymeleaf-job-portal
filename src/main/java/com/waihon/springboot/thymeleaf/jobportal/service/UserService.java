ffcfb4e4ec9d3f79fd93b1955db90504799fbdb5
Waihon Yew <yewwaihon@gmail.com>
2025-03-09 17:10:33 +0800

    Get a user by email address

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/UserService.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,6 +23,10 @@ public class UserService {
         user.setRegistrationDate(new Date(System.currentTimeMillis()));
 
         return userRepository.save(user);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
 }
