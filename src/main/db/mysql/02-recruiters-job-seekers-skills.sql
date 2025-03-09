c0141b4b605216ebce9158724ea44fc616d44cb1
Waihon Yew <yewwaihon@gmail.com>
2025-03-09 18:02:33 +0800

    Tables: Use custom name for constraints

--- src/main/db/mysql/02-recruiters-job-seekers-skills.sql
+++ src/main/db/mysql/02-recruiters-job-seekers-skills.sql
@@ -10,7 +10,7 @@ CREATE TABLE `recruiter_profiles` (
   `profile_photo` varchar(64) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_account_id`),
-  CONSTRAINT `FK42q4eb7jw1bvw3oy83vc05ft6` FOREIGN KEY (`user_account_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `fk_recruiter_profiles_user_account_id` FOREIGN KEY (`user_account_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `job_seeker_profiles` (
@@ -25,7 +25,7 @@ CREATE TABLE `job_seeker_profiles` (
   `state` varchar(255) DEFAULT NULL,
   `work_authorization` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_account_id`),
-  CONSTRAINT `FKohp1poe14xlw56yxbwu2tpdm7` FOREIGN KEY (`user_account_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `fk_job_seeker_profiles_user_account_id` FOREIGN KEY (`user_account_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `skills` (
@@ -35,6 +35,6 @@ CREATE TABLE `skills` (
   `years_of_experience` varchar(255) DEFAULT NULL,
   `job_seeker_profile` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKsjdksau8sat30c00aqh5xf2wh` (`job_seeker_profile`),
-  CONSTRAINT `FKsjdksau8sat30c00aqh5xf2wh` FOREIGN KEY (`job_seeker_profile`) REFERENCES `job_seeker_profile` (`user_account_id`)
+  KEY `fk_skills_job_seeker_profile` (`job_seeker_profile`),
+  CONSTRAINT `fk_skills_job_seeker_profile` FOREIGN KEY (`job_seeker_profile`) REFERENCES `job_seeker_profiles` (`user_account_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
