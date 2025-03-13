335de1e20436dbb2b73ab4a525afd6456187dc94
Waihon Yew <yewwaihon@gmail.com>
2025-03-13 08:39:55 +0800

    Create RecruiterProfileRepository extending JpaRepository

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,6 @@
+package com.waihon.springboot.thymeleaf.jobportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfileRepository, Integer> {
+}
