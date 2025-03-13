de07ae11ae82ec950ae5bd0ce6386a9a7e75f477
Waihon Yew <yewwaihon@gmail.com>
2025-03-14 07:23:21 +0800

    Fix declaration of RecruiterProfileRepository

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/repository/RecruiterProfileRepository.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/repository/RecruiterProfileRepository.java
@@ -1,6 +1,7 @@
 package com.waihon.springboot.thymeleaf.jobportal.repository;
 
+import com.waihon.springboot.thymeleaf.jobportal.entity.RecruiterProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfileRepository, Integer> {
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
 }
