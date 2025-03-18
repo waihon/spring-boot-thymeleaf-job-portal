4e7c4785af78ee6915cc01a97407f2f21a7021f1
Waihon Yew <yewwaihon@gmail.com>
2025-03-18 15:54:14 +0800

    Encrypt user password during registration

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/UserService.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/UserService.java
@@ -7,6 +7,7 @@ import com.waihon.springboot.thymeleaf.jobportal.repository.JobSeekerProfileRepo
 import com.waihon.springboot.thymeleaf.jobportal.repository.RecruiterProfileRepository;
 import com.waihon.springboot.thymeleaf.jobportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,18 +19,22 @@ public class UserService {
     private final UserRepository userRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository, JobSeekerProfileRepository jobSeekerProfileRepository,
-                       RecruiterProfileRepository recruiterProfileRepository) {
+                       RecruiterProfileRepository recruiterProfileRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User addNew(User user) {
         user.setActive(true);
         user.setRegistrationDate(new Date(System.currentTimeMillis()));
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // encrypt user password
         User savedUser = userRepository.save(user);
 
         int userTypeId = user.getUserType().getUserTypeId();
