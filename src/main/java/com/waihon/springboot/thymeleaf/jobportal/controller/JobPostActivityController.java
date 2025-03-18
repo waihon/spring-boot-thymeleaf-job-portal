1fc0c30c0e87cacfb8018398c9488e495009720d
Waihon Yew <yewwaihon@gmail.com>
2025-03-18 18:44:57 +0800

    Integrate dashboard with current logged in user

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobPostActivityController.java
@@ -0,0 +1,37 @@
+package com.waihon.springboot.thymeleaf.jobportal.controller;
+
+import com.waihon.springboot.thymeleaf.jobportal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobPostActivityController {
+
+    private final UserService userService;
+
+    @Autowired
+    public JobPostActivityController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/dashboard")
+    public String searchJobs(Model model) {
+        Object currentUserProfile = userService.getCurrentUserProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            model.addAttribute("username", currentUsername);
+        }
+
+        model.addAttribute("user", currentUserProfile);
+
+        return "dashboard";
+    }
+
+}
