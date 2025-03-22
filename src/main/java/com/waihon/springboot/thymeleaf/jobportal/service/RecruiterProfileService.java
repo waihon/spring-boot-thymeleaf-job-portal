58d6e1d25e7ce53c17bc9d9540de5b0784defd7f
Waihon Yew <yewwaihon@gmail.com>
2025-03-22 09:46:43 +0800

    Create Recruiter Profile class

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/RecruiterProfileService.java
@@ -0,0 +1,25 @@
+package com.waihon.springboot.thymeleaf.jobportal.service;
+
+import com.waihon.springboot.thymeleaf.jobportal.entity.RecruiterProfile;
+import com.waihon.springboot.thymeleaf.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+
+}
