d2408159835324c78cf535399bc0374a4adea7ad
Waihon Yew <yewwaihon@gmail.com>
2025-03-23 23:00:30 +0800

    Automatically login upon successful registration

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/config/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import com.waihon.springboot.thymeleaf.jobportal.service.CustomUserDetailsServic
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -84,6 +86,12 @@ public class WebSecurityConfig {
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
