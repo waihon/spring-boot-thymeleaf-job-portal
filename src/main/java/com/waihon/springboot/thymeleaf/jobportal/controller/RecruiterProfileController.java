c99e92002151ebfcc7ed2404bdbb9d64805ee420
Waihon Yew <yewwaihon@gmail.com>
2025-03-25 16:51:10 +0800

    Hander to add new Recruiter Profile

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/RecruiterProfileController.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/RecruiterProfileController.java
@@ -4,16 +4,24 @@ import com.waihon.springboot.thymeleaf.jobportal.entity.RecruiterProfile;
 import com.waihon.springboot.thymeleaf.jobportal.entity.User;
 import com.waihon.springboot.thymeleaf.jobportal.repository.UserRepository;
 import com.waihon.springboot.thymeleaf.jobportal.service.RecruiterProfileService;
+import com.waihon.springboot.thymeleaf.jobportal.util.FileUploadUtil;
+import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -35,7 +43,8 @@ public class RecruiterProfileController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
-            User user = userRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+            User user = userRepository.findByEmail(currentUsername)
+                    .orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
 
             Optional<RecruiterProfile> recruiterProfile = recruiterProfileService.getOne(user.getUserId());
             recruiterProfile.ifPresent(profile ->
@@ -43,6 +52,44 @@ public class RecruiterProfileController {
         }
 
         return "recruiter-profile";
+    }
+
+    @PostMapping("/add-new")
+    public String addNew(RecruiterProfile recruiterProfile,
+                         @RequestParam("image")MultipartFile multipartFile,
+                         Model model) {
+        // Associate recruiter profile with existing user account
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof  AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            User user = userRepository.findByEmail(currentUsername)
+                    .orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+            recruiterProfile.setUser(user);
+            recruiterProfile.setUserAccountId(user.getUserId());
+        }
+
+        model.addAttribute("profile", recruiterProfile);
+
+        // Set image name in recruiter profile
+        String fileName = "";
+        if (!multipartFile.getOriginalFilename().equals("")) {
+            fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            recruiterProfile.setProfilePhoto(fileName);
+        }
+
+        // Save recruiter profile to DB
+        RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+
+        // Read profile image from request's multipart file and save image
+        // on the server in directory: photos/recruiter
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+        try {
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return "redirect:/dashboard";
     }
 
 }
