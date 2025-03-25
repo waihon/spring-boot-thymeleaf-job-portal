9aa3545651856c898bee69d04c0a074754fc3977
Waihon Yew <yewwaihon@gmail.com>
2025-03-25 16:19:52 +0800

    Upload multipart file to a directory on file system

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/util/FileUploadUtil.java
@@ -0,0 +1,30 @@
+package com.waihon.springboot.thymeleaf.jobportal.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileUploadUtil {
+
+    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path path = uploadPath.resolve(fileName);
+            System.out.println("File path: " + path);
+            System.out.println("File name: " + fileName);
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+
+}
