99b7d7951c1a36a44bc010ed16e6f099bb361e18
Waihon Yew <yewwaihon@gmail.com>
2025-03-26 21:13:21 +0800

    SQL for creating Job Companies, Locations & Post Activities

---
+++ src/main/db/mysql/03-job-companies-locations-post-activities.sql
@@ -0,0 +1,38 @@
+USE `jobportal`;
+
+CREATE TABLE `job_company` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `logo` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `job_location` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `job_post_activity` (
+  `job_post_id` int NOT NULL AUTO_INCREMENT,
+  `description_of_job` varchar(10000) DEFAULT NULL,
+  `job_title` varchar(255) DEFAULT NULL,
+  `job_type` varchar(255) DEFAULT NULL,
+  `posted_date` datetime(6) DEFAULT NULL,
+  `remote` varchar(255) DEFAULT NULL,
+  `salary` varchar(255) DEFAULT NULL,
+  `job_company_id` int DEFAULT NULL,
+  `job_location_id` int DEFAULT NULL,
+  `posted_by_id` int DEFAULT NULL,
+  PRIMARY KEY (`job_post_id`),
+  KEY `FKpjpv059hollr4tk92ms09s6is` (`job_company_id`),
+  KEY `FK44003mnvj29aiijhsc6ftsgxe` (`job_location_id`),
+  KEY `FK62yqqbypsq2ik34ngtlw4m9k3` (`posted_by_id`),
+  CONSTRAINT `FK44003mnvj29aiijhsc6ftsgxe` FOREIGN KEY (`job_location_id`) REFERENCES `job_location` (`id`),
+  CONSTRAINT `FK62yqqbypsq2ik34ngtlw4m9k3` FOREIGN KEY (`posted_by_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FKpjpv059hollr4tk92ms09s6is` FOREIGN KEY (`job_company_id`) REFERENCES `job_company` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+:w
