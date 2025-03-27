054823c049e6e8394e4b77884eedae92d76c7af9
Waihon Yew <yewwaihon@gmail.com>
2025-03-27 22:24:32 +0800

    Standardize on using plural for name of tables

--- src/main/db/mysql/03-job-companies-locations-post-activities.sql
+++ src/main/db/mysql/03-job-companies-locations-post-activities.sql
@@ -1,6 +1,6 @@
 USE `jobportal`;
 
-CREATE TABLE `job_company` (
+CREATE TABLE `job_companies` (
   `id` int NOT NULL AUTO_INCREMENT,
   `logo` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE `job_company` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `job_location` (
+CREATE TABLE `job_locations` (
   `id` int NOT NULL AUTO_INCREMENT,
   `city` varchar(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE `job_location` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `job_post_activity` (
+CREATE TABLE `job_post_activities` (
   `job_post_id` int NOT NULL AUTO_INCREMENT,
   `description_of_job` varchar(10000) DEFAULT NULL,
   `job_title` varchar(255) DEFAULT NULL,
@@ -31,8 +31,7 @@ CREATE TABLE `job_post_activity` (
   KEY `FKpjpv059hollr4tk92ms09s6is` (`job_company_id`),
   KEY `FK44003mnvj29aiijhsc6ftsgxe` (`job_location_id`),
   KEY `FK62yqqbypsq2ik34ngtlw4m9k3` (`posted_by_id`),
-  CONSTRAINT `FK44003mnvj29aiijhsc6ftsgxe` FOREIGN KEY (`job_location_id`) REFERENCES `job_location` (`id`),
+  CONSTRAINT `FK44003mnvj29aiijhsc6ftsgxe` FOREIGN KEY (`job_location_id`) REFERENCES `job_locations` (`id`),
   CONSTRAINT `FK62yqqbypsq2ik34ngtlw4m9k3` FOREIGN KEY (`posted_by_id`) REFERENCES `users` (`user_id`),
-  CONSTRAINT `FKpjpv059hollr4tk92ms09s6is` FOREIGN KEY (`job_company_id`) REFERENCES `job_company` (`id`)
+  CONSTRAINT `FKpjpv059hollr4tk92ms09s6is` FOREIGN KEY (`job_company_id`) REFERENCES `job_companies` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-:w
