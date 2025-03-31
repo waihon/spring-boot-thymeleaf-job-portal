718bd29be19ac9540434616474f833c4f6ddbcc9
Waihon Yew <yewwaihon@gmail.com>
2025-03-31 18:42:57 +0800

    Service to return job posts by recruiter ID

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/JobPostActivityService.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/JobPostActivityService.java
@@ -1,9 +1,12 @@
 package com.waihon.springboot.thymeleaf.jobportal.service;
 
-import com.waihon.springboot.thymeleaf.jobportal.entity.JobPostActivity;
+import com.waihon.springboot.thymeleaf.jobportal.entity.*;
 import com.waihon.springboot.thymeleaf.jobportal.repository.JobPostActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -17,5 +20,18 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobDto> getRecruiterJobs(int recruiter) {
+        List<IRecruiterJob> recruiterJobs = jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobDto> recruiterJobDtos = new ArrayList<>();
+
+        for (IRecruiterJob job : recruiterJobs) {
+            JobLocation location = new JobLocation(job.getLocationId(), job.getCity(), job.getState(), job.getCountry());
+            JobCompany company = new JobCompany(job.getCompanyId(), job.getName(), "");
+            recruiterJobDtos.add(new RecruiterJobDto(job.getTotalCandidates(), job.getJobPostId(), job.getJobTitle(), location, company));
+        }
+
+        return recruiterJobDtos;
     }
 }
