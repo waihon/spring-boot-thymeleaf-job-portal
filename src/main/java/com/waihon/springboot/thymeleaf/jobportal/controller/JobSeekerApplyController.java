c5ae79596d031233bea86e62446eb43781528d2a
Waihon Yew <yewwaihon@gmail.com>
2025-04-01 10:39:02 +0800

    Hander to display job details

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobSeekerApplyController.java
@@ -0,0 +1,33 @@
+package com.waihon.springboot.thymeleaf.jobportal.controller;
+
+import com.waihon.springboot.thymeleaf.jobportal.entity.JobPostActivity;
+import com.waihon.springboot.thymeleaf.jobportal.service.JobPostActivityService;
+import com.waihon.springboot.thymeleaf.jobportal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class JobSeekerApplyController {
+
+    private final JobPostActivityService jobPostActivityService;
+    private final UserService userService;
+
+    @Autowired
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UserService userService) {
+        this.jobPostActivityService = jobPostActivityService;
+        this.userService = userService;
+    }
+
+    @GetMapping("job-details-apply/{id}")
+    public String display(@PathVariable("id") int id, Model model) {
+        JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+
+        model.addAttribute("jobDetails", jobDetails);
+        model.addAttribute("user", userService.getCurrentUserProfile());
+
+        return "job-details";
+    }
+}
