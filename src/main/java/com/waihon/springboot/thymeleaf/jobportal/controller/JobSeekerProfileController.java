6aa46dd4628c5676837fae8da4e2014070484250
Waihon Yew <yewwaihon@gmail.com>
2025-04-05 00:27:37 +0800

    Controller handler to add new or save job seeker profile

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobSeekerProfileController.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobSeekerProfileController.java
@@ -5,17 +5,24 @@ import com.waihon.springboot.thymeleaf.jobportal.entity.Skill;
 import com.waihon.springboot.thymeleaf.jobportal.entity.User;
 import com.waihon.springboot.thymeleaf.jobportal.repository.UserRepository;
 import com.waihon.springboot.thymeleaf.jobportal.service.JobSeekerProfileService;
+import com.waihon.springboot.thymeleaf.jobportal.util.FileUploadUtil;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -54,6 +61,60 @@ public class JobSeekerProfileController {
         }
 
         return "job-seeker-profile";
+    }
+
+    @PostMapping("/add-new")
+    public String addNew(JobSeekerProfile jobSeekerProfile,
+                         @RequestParam("image") MultipartFile image,
+                         @RequestParam("pdf") MultipartFile pdf,
+                         Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            User user = userRepository.findByEmail(authentication.getName())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+            jobSeekerProfile.setUser(user);
+            jobSeekerProfile.setUserAccountId(user.getUserId());
+        }
+
+        List<Skill> skillsList = new ArrayList<>();
+
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skillsList);
+
+        for (Skill skill : jobSeekerProfile.getSkills()) {
+            skill.setJobSeekerProfile(jobSeekerProfile);
+        }
+
+        String imageName = "";
+        String resumeName = "";
+
+        if (!Objects.equals(image.getOriginalFilename(), "")) {
+            imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
+            jobSeekerProfile.setProfilePhoto(imageName);
+        }
+
+        if (!Objects.equals(pdf.getOriginalFilename(), "")) {
+            resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
+            jobSeekerProfile.setResume(resumeName);
+        }
+
+        JobSeekerProfile seekerProfile = jobSeekerProfileService.addNew(jobSeekerProfile);
+
+        try {
+            String uploadDir = "photos/candidate/" + jobSeekerProfile.getUserAccountId();
+            if (!Objects.equals(image.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, imageName, image);
+            }
+            if (!Objects.equals(pdf.getOriginalFilename(), "")) {
+                FileUploadUtil.saveFile(uploadDir, resumeName, pdf);
+            }
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return "redirect:/dashboard";
     }
 
 }
