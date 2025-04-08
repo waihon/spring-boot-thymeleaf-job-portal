2b2dd461064adc9631fda05491f18ae54c1d7bd8
Waihon Yew <yewwaihon@gmail.com>
2025-04-08 23:16:42 +0800

    Specify job_seeker_applies as the table name

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/entity/JobSeekerApply.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/entity/JobSeekerApply.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(uniqueConstraints = {
+@Table(name = "job_seeker_applies", uniqueConstraints = {
     @UniqueConstraint(columnNames = {"user", "job"})
 })
 public class JobSeekerApply implements Serializable {
