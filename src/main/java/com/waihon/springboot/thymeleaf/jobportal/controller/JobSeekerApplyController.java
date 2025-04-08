a118d08c10efda5cfa753f95804e291fdba0dcd9
Waihon Yew <yewwaihon@gmail.com>
2025-04-08 23:39:53 +0800

    Controller handler to show Job Details

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobSeekerApplyController.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/controller/JobSeekerApplyController.java
@@ -1,29 +1,74 @@
 package com.waihon.springboot.thymeleaf.jobportal.controller;
 
-import com.waihon.springboot.thymeleaf.jobportal.entity.JobPostActivity;
-import com.waihon.springboot.thymeleaf.jobportal.service.JobPostActivityService;
-import com.waihon.springboot.thymeleaf.jobportal.service.UserService;
+import com.waihon.springboot.thymeleaf.jobportal.entity.*;
+import com.waihon.springboot.thymeleaf.jobportal.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class JobSeekerApplyController {
 
     private final JobPostActivityService jobPostActivityService;
     private final UserService userService;
+    private final JobSeekerApplyService jobSeekerApplyService;
+    private final RecruiterProfileService recruiterProfileService;
+    private final JobSeekerProfileService jobSeekerProfileService;
 
     @Autowired
-    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UserService userService) {
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService,
+                                    UserService userService,
+                                    JobSeekerApplyService jobSeekerApplyService,
+                                    RecruiterProfileService recruiterProfileService,
+                                    JobSeekerProfileService jobSeekerProfileService) {
         this.jobPostActivityService = jobPostActivityService;
         this.userService = userService;
+        this.jobSeekerApplyService = jobSeekerApplyService;
+        this.recruiterProfileService = recruiterProfileService;
+        this.jobSeekerProfileService = jobSeekerProfileService;
     }
 
     @GetMapping("job-details-apply/{id}")
     public String display(@PathVariable("id") int id, Model model) {
         JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+        List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getJobCandidates(jobDetails);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                RecruiterProfile  user = recruiterProfileService.getCurrentRecruiterProfile();
+                if (user != null) {
+                    model.addAttribute("applyList", jobSeekerApplyList);
+                }
+            } else {
+                JobSeekerProfile user = jobSeekerProfileService.getCurrentSeekerProfile();
+                if (user != null) {
+                    boolean applied = false;
+                    for (JobSeekerApply jobSeekerApply : jobSeekerApplyList) {
+                        if (jobSeekerApply.getUser().getUserAccountId() == user.getUserAccountId()) {
+                            applied = true;
+                            break;
+                        }
+                    }
+                    model.addAttribute("alreadyApplied", applied);
+                }
+            }
+        }
+
+        JobSeekerApply jobSeekerApply = new JobSeekerApply();
+        model.addAttribute("applyJob", jobSeekerApply);
 
         model.addAttribute("jobDetails", jobDetails);
         model.addAttribute("user", userService.getCurrentUserProfile());
