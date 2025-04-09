0ebacd51aadbee0f67b3ef6833218e059c417525
Waihon Yew <yewwaihon@gmail.com>
2025-04-09 23:49:48 +0800

    Create Job Seeker Save service class

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/service/JobSeekerSaveService.java
@@ -0,0 +1,28 @@
+package com.waihon.springboot.thymeleaf.jobportal.service;
+
+import com.waihon.springboot.thymeleaf.jobportal.entity.JobPostActivity;
+import com.waihon.springboot.thymeleaf.jobportal.entity.JobSeekerProfile;
+import com.waihon.springboot.thymeleaf.jobportal.entity.JobSeekerSave;
+import com.waihon.springboot.thymeleaf.jobportal.repository.JobSeekerSaveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerSaveService {
+
+    private final JobSeekerSaveRepository jobSeekerSaveRepository;
+
+    public JobSeekerSaveService(JobSeekerSaveRepository jobSeekerSaveRepository) {
+        this.jobSeekerSaveRepository = jobSeekerSaveRepository;
+    }
+
+    public List<JobSeekerSave> getCandidateJobs(JobSeekerProfile user) {
+        return jobSeekerSaveRepository.findByUser(user);
+    }
+
+    public List<JobSeekerSave> getJobCandidates(JobPostActivity job) {
+        return jobSeekerSaveRepository.findByJob(job);
+    }
+
+}
