de2edb2697776422fba225653e7eeec7b05e57d5
Waihon Yew <yewwaihon@gmail.com>
2025-04-09 22:41:10 +0800

    Use custom name for indexes and constraints of job_seeker_saves

--- src/main/db/mysql/05-job-seeker-saves.sql
+++ src/main/db/mysql/05-job-seeker-saves.sql
@@ -5,8 +5,8 @@ CREATE TABLE `job_seeker_saves` (
   `job_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK1vn1w4dxfiavb5q2gu1n0whxo` (`user_id`,`job_id`),
-  KEY `FKpb44x040gkdltxqy9m7jmvvf3` (`job_id`),
-  CONSTRAINT `FK96dyvgd8hmdohqsfdpvyl89mg` FOREIGN KEY (`user_id`) REFERENCES `job_seeker_profiles` (`user_account_id`),
-  CONSTRAINT `FKpb44x040gkdltxqy9m7jmvvf3` FOREIGN KEY (`job_id`) REFERENCES `job_post_activities` (`job_post_id`)
+  UNIQUE KEY `uk_job_seeker_saves_user_id_job_id` (`user_id`,`job_id`),
+  KEY `fk_job_seeker_saves_job_id` (`job_id`),
+  CONSTRAINT `fk_job_seeker_saves_user_id` FOREIGN KEY (`user_id`) REFERENCES `job_seeker_profiles` (`user_account_id`),
+  CONSTRAINT `fk_job_seeker_saves_job_id` FOREIGN KEY (`job_id`) REFERENCES `job_post_activities` (`job_post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
