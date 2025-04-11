66c753037ecb792059752c37ce4a1c1a2d27a4fa
Waihon Yew <yewwaihon@gmail.com>
2025-04-12 00:49:12 +0800

    File download utility to get file as resource

---
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/util/FileDownloadUtil.java
@@ -0,0 +1,30 @@
+package com.waihon.springboot.thymeleaf.jobportal.util;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDownloadUtil {
+
+    private Path foundFile;
+
+    public Resource getFileAsResource(String downloadDir, String fileName) throws IOException {
+        Path path = Paths.get(downloadDir);
+        Files.list(path).forEach(file -> {
+           if (file.getFileName().toString().startsWith(fileName)) {
+               foundFile = file;
+           }
+        });
+
+        if (foundFile != null) {
+            return new UrlResource(foundFile.toUri());
+        }
+
+        return null;
+    }
+}
