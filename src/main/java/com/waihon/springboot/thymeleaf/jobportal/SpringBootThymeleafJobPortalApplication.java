8d6dd7987e05e441aaf19956f424a87ee3e8fa80
Waihon Yew <yewwaihon@gmail.com>
2025-04-12 22:56:03 +0800

    Enabled hidden methods such as delete

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/SpringBootThymeleafJobPortalApplication.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/SpringBootThymeleafJobPortalApplication.java
@@ -2,12 +2,19 @@ package com.waihon.springboot.thymeleaf.jobportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SpringBootThymeleafJobPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafJobPortalApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
 	}
 
 }
