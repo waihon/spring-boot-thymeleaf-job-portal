72178aadc3b82cad44bae1d33c5868e812469421
Waihon Yew <yewwaihon@gmail.com>
2025-04-12 18:21:44 +0800

    Fix profile image not displayed on Job Details page

--- src/main/java/com/waihon/springboot/thymeleaf/jobportal/entity/JobSeekerProfile.java
+++ src/main/java/com/waihon/springboot/thymeleaf/jobportal/entity/JobSeekerProfile.java
@@ -158,7 +158,7 @@ public class JobSeekerProfile {
     public String getPhotoImagePath() {
         if (profilePhoto == null || userAccountId == null) return null;
 
-        return "photos/candidate/" + userAccountId + "/" + profilePhoto;
+        return "/photos/candidate/" + userAccountId + "/" + profilePhoto;
     }
 
     @Override
